3a61d59dfc08ab4455e56be92792be158ee00c75
leilay <zoney@163.com>
2017-09-29 14:06:29 +0800

    test

---
+++ Demos/listTemplate/list-template.js
@@ -0,0 +1,19 @@
+Page({
+  data: {
+     ListBlock: {
+            items: [
+                {
+                    "icon": "https://zos.alipayobjects.com/rmsportal/HMVRwQoJUDyxZVkTyIqF.png",
+                    "title": "title"
+                },
+                {
+                    "icon": "https://zos.alipayobjects.com/rmsportal/HMVRwQoJUDyxZVkTyIqF.png",
+                    "title": "title"
+                }
+            ],
+            columnNum: 4
+        }
+        
+  },
+  onLoad() {},
+});
