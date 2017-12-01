16a58edb6c095731c1a9de8a69ea47b7e7aa95dd
leilay <zoney@163.com>
2017-12-01 09:08:33 +0800

    fix something

---
+++ wx/pages/biz/goods-detail/goods-detail.js
@@ -0,0 +1,74 @@
+// pages/biz/goods-detail/goods-detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      '//img10.360buyimg.com/n1/jfs/t3235/143/2924985643/316383/370982db/57e8d527Ncb5ce343.jpg',
+      '//img12.360buyimg.com/n1/jfs/t7339/187/2408921911/411414/aaa18686/59ae7f9bNee021f63.jpg',
+      '//img12.360buyimg.com/n1/jfs/t3256/318/2464726152/379105/30695126/57e24abeNa8cc9cb3.jpg',
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- wx/pages/component/tabs/tabs.js
+++ wx/pages/component/tabs/tabs.js
@@ -2,22 +2,5 @@
 Page({
   data: {
 
-    // tab切换
-    currentTab: 0,
   },
-
-  changeTab: function (e) {
-    this.setData({ currentTab: e.detail.current });
-  },
-
-  swichTab: function (e) {
-    var that = this;
-    if (this.data.currentTab === e.target.dataset.current) {
-      return false;
-    } else {
-      that.setData({
-        currentTab: e.target.dataset.current
-      })
-    }
-  }
 })
